d1d94a00fbc4830bc3b9fe295c6eba4de6074f13
A <fregate@ngs.ru>
2017-01-13 21:14:34 +0700

    add animations and fix some bugs

--- vs/PhaserTest001/app.ts
+++ vs/PhaserTest001/app.ts
@@ -1,8 +1,24 @@
-﻿module GameCurling {
+module GameCurling {
 
     class SETTINGS {
         SCREEN_WIDTH: number = 600;
         SCREEN_HEIGHT: number = 800;
+    };
+
+    class ANIMATION_WAITER {
+        private refs: number = 0;
+
+        addRef(cb: Function, ctx: any) {
+            this.refs++;
+            if (cb !== null && typeof cb == 'function')
+                cb.apply(ctx);
+        }
+
+        releaseRef(cb: Function, ctx: any) {
+            this.refs--;
+            if (this.refs == 0 && cb !== null && typeof cb == 'function')
+                cb.apply(ctx);
+        }
     };
 
     interface FieldCoord {
@@ -21,6 +37,8 @@
     };
 
     export class TitleScreenState extends Phaser.State {
+        private spaceKey: Phaser.Key;
+
         game: Phaser.Game;
 
         constructor() {
@@ -34,6 +52,9 @@
         create() {
             this.game.add.sprite(0, 0, "title");
             this.input.onTap.addOnce(this.titleClicked, this);
+
+            this.spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+            this.spaceKey.onDown.add(this.titleClicked, this);
         }
 
         titleClicked() {
@@ -42,6 +63,8 @@
     };
 
     export class EndGameScreenState extends Phaser.State {
+        private spaceKey: Phaser.Key;
+
         game: Phaser.Game;
         textGameOver: Phaser.Text;
         textRecord: Phaser.Text;
@@ -63,7 +86,7 @@
         create() {
             this.game.add.sprite(0, 0, "bg");
 
-            var style = { font: "bold 65px Courier", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+            let style = { font: "bold 65px Courier", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
             this.textGameOver = this.game.add.text(0, 0, "ВОТ И ВСЕ", style);
             this.textGameOver.setTextBounds(0, 150, this.game.width, 100);
 
@@ -71,7 +94,7 @@
             this.textRecord = this.game.add.text(0, 0, "Камней сломалось: " + this.points, style);
             this.textRecord.setTextBounds(0, 250, this.game.width, 50);
 
-            var styleoverall = { font: "20px Courier", fill: "#fff", align: "left", wordWrap: true, wordWrapWidth: this.game.width - 30 };
+            let styleoverall = { font: "20px Courier", fill: "#fff", align: "left", wordWrap: true, wordWrapWidth: this.game.width - 30 };
             this.textRecord = this.game.add.text(30, 350, "Не стоит отчаиваться! Если что, то приезжай в керлинг-клуб \
 'Пингвин' и там сможешь по- настоящему покатать камни и потереть щеткой!\n\n\
 Жду тебя в субботу к 13:30 прямо там (это что бы время было переодеться).\n\
@@ -79,6 +102,9 @@
 Возьми удобную одежду - это же спорт!", styleoverall);
 
             this.input.onTap.addOnce(this.titleClicked, this);
+
+            this.spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+            this.spaceKey.onDown.add(this.titleClicked, this);
         }
 
         titleClicked() {
@@ -103,13 +129,10 @@
         private fieldSprites: SpriteKey[];
         private sprtId: number;
 
+        private lockInput: boolean = false;
+        private aw: ANIMATION_WAITER = new ANIMATION_WAITER;
+
         constructor() {
-/*            this.game = new Phaser.Game(this.SCREEN_WIDTH, this.SCREEN_HEIGHT, Phaser.AUTO, 'gamefield',
-                {
-                    preload: this.preload,
-                    create: this.create,
-                    update: this.update
-                }); */
             super();
         }
 
@@ -124,11 +147,15 @@
 
         points: number;
 
-
         // handle input function
         HandleTouchMouse(pointer) {
+            if (this.lockInput) {
+                return;
+            }
+
             if (this.bottomRect.contains(pointer.x, pointer.y)) {
-                this.DropRowDown();
+//                this.DropRowDown();
+                this.DropBlocks();
             }
 
             if (this.topLeftRect.contains(pointer.x, pointer.y)) {
@@ -141,138 +168,185 @@
         }
 
         ShiftRowLeft() {
-            var shifted = this.row.shift();
+            let shifted = this.row.shift();
             this.row.push(shifted);
             this.row.forEach((spr, idx) => {
-                spr.position.set(
+                this.TweenSpritePosition(spr,
                     this.OFFSET_FIELD + this.TILE_SPACE + idx * (this.TILE_SPACE + this.TILE_SIZE),
                     this.TILE_SPACE);
             }, this);
         }
 
         ShiftRowRight() {
-            var popped = this.row.pop();
+            let popped = this.row.pop();
             this.row.unshift(popped);
             this.row.forEach((spr, idx) => {
-                spr.position.set(
+                this.TweenSpritePosition(spr,
                     this.OFFSET_FIELD + this.TILE_SPACE + idx * (this.TILE_SPACE + this.TILE_SIZE),
                     this.TILE_SPACE);
             }, this);
         }
 
-        DropRowDown() {
-            // drop stones into field
+        DropBlocks() {
             this.row.forEach((spr, idx) => {
-                this.field[idx] ? this.field[idx] : this.field[idx] = [];
-                var fcy = this.field[idx].filter((num) => { return num.color >= 0; }).length;
+                this.field[idx] ? this.field[idx] : this.field[idx] = []; // allocate new line
+                let fcy = this.field[idx].filter((num) => { return num.color >= 0; }).length;
                 this.field[idx][fcy] = { color: parseInt(spr.key.toString()[1]), key: this.sprtId };
 
                 spr.anchor.set(0, 1);
-                spr.position.y = this.SCREEN_HEIGHT - this.TILE_SPACE - (fcy * (this.TILE_SPACE + this.TILE_SIZE));
-                this.fieldSprites.push({ key: this.sprtId, sprt: spr });
-                this.sprtId += 1;
+                this.TweenSpritePosition(
+                    spr,
+                    spr.position.x,
+                    this.SCREEN_HEIGHT - this.TILE_SPACE - (fcy * (this.TILE_SPACE + this.TILE_SIZE)),
+                    null,
+                    this.RemoveEmptySpaces
+                );
+                this.fieldSprites.push({ key: this.sprtId++, sprt: spr });
             }, this);
-            this.row = [];
+            this.row = []; // clear input blocks
+        }
 
-            // check field
-            while (1) {
-                var bFieldChecked = true;
-                for (var x = 0; x < this.TILE_COLUMNS; x++) {
-                    for (var y = 0; y < this.TILE_ROWS; y++) {
-                        if (!this.field[x] || !this.field[x][y] || this.field[x][y].color < 0)
+        RemoveEmptySpaces() {
+            this.field.forEach((line, fldidx) => {
+                line = line.filter((val) => { return val.color >= 0; });
+                this.field[fldidx] = line;
+                line.forEach((val, idx) => {
+                    var spr = this.fieldSprites.filter((s) => { return s.key == val.key; }, this)[0].sprt;
+                    this.TweenSpritePosition(
+                        spr,
+                        spr.position.x,
+                        this.SCREEN_HEIGHT - this.TILE_SPACE - (idx * (this.TILE_SPACE + this.TILE_SIZE)),
+                        null,
+                        this.CheckField
+                    );
+                }, this);
+            }, this);
+        }
+
+        CheckField() {
+            let localPoints: number = 0;
+            for (let x = 0; x < this.TILE_COLUMNS; x++) {
+                for (let y = 0; y < this.TILE_ROWS; y++) {
+                    if (!this.field[x] || !this.field[x][y] || this.field[x][y].color < 0)
+                        continue;
+
+                    let stack = [];
+                    let fc: FieldCoord = { x: x, y: y };
+                    stack.push(fc);
+                    let colorspace = [];
+                    while (stack.length) {
+                        let ccc = stack.pop();
+                        if (!ccc)
                             continue;
 
-                        var stack = [];
-                        var fc: FieldCoord = { x: x, y: y };
-                        stack.push(fc);
-                        var colorspace = [];
-                        while (stack.length) {
-                            var ccc = stack.pop();
-                            if (!ccc)
-                                continue;
+                        let fval = this.field[ccc.x][ccc.y].color;
+                        colorspace.push(ccc);
 
-                            var fval = this.field[ccc.x][ccc.y].color;
-                            colorspace.push(ccc);
-
-                            // check north
-                            if ((ccc.y < this.TILE_ROWS - 1) && (this.field[ccc.x][ccc.y + 1] && this.field[ccc.x][ccc.y + 1].color == fval)) {
-                                var nc: FieldCoord = { x: ccc.x, y: ccc.y + 1 };
-                                // check for not in colorspace
-                                if (colorspace.filter((csc, idx) => { return csc.x == nc.x && csc.y == nc.y; }, this).length == 0)
-                                    stack.push(nc);
-                            }
-
-                            // check south
-                            if (ccc.y > 0 && (this.field[ccc.x][ccc.y - 1] && this.field[ccc.x][ccc.y - 1].color == fval)) {
-                                var nc: FieldCoord = { x: ccc.x, y: ccc.y - 1 };
-                                if (colorspace.filter((csc, idx) => { return csc.x == nc.x && csc.y == nc.y; }, this).length == 0)
-                                    stack.push(nc);
-                            }
-
-                            // check west
-                            if (ccc.x > 0 && (this.field[ccc.x - 1][ccc.y] && this.field[ccc.x - 1][ccc.y].color == fval)) {
-                                var nc: FieldCoord = { x: ccc.x - 1, y: ccc.y };
-                                if (colorspace.filter((csc, idx) => { return csc.x == nc.x && csc.y == nc.y; }, this).length == 0)
-                                    stack.push(nc);
-                            }
-
-                            // check east
-                            if ((ccc.x < this.TILE_COLUMNS - 1) && (this.field[ccc.x + 1][ccc.y] && this.field[ccc.x + 1][ccc.y].color == fval)) {
-                                var nc: FieldCoord = { x: ccc.x + 1, y: ccc.y };
-                                if (colorspace.filter((csc, idx) => { return csc.x == nc.x && csc.y == nc.y; }, this).length == 0)
-                                    stack.push(nc);
-                            }
+                        // check north
+                        if ((ccc.y < this.TILE_ROWS - 1) && (this.field[ccc.x][ccc.y + 1] && this.field[ccc.x][ccc.y + 1].color == fval)) {
+                            var nc: FieldCoord = { x: ccc.x, y: ccc.y + 1 };
+                            // check for not in colorspace
+                            if (colorspace.filter((csc, idx) => { return csc.x == nc.x && csc.y == nc.y; }, this).length == 0)
+                                stack.push(nc);
                         }
 
-                        if (colorspace.length < 3)
-                            continue;
+                        // check south
+                        if (ccc.y > 0 && (this.field[ccc.x][ccc.y - 1] && this.field[ccc.x][ccc.y - 1].color == fval)) {
+                            var nc: FieldCoord = { x: ccc.x, y: ccc.y - 1 };
+                            if (colorspace.filter((csc, idx) => { return csc.x == nc.x && csc.y == nc.y; }, this).length == 0)
+                                stack.push(nc);
+                        }
 
-                        bFieldChecked = false;
-                        colorspace.forEach((ccc) => {
-                            var fval = this.field[ccc.x][ccc.y];
-                            fval.color = -1;
-                            var idx = -1;
-                            this.fieldSprites.filter((val, i) => {
-                                if (fval.key == val.key) {
-                                    idx = i;
-                                    return true;
-                                }
-                                return false;
-                            }, this)[0].sprt.destroy();
-                            this.fieldSprites.splice(idx, 1);
-                        }, this);
+                        // check west
+                        if (ccc.x > 0 && (this.field[ccc.x - 1][ccc.y] && this.field[ccc.x - 1][ccc.y].color == fval)) {
+                            var nc: FieldCoord = { x: ccc.x - 1, y: ccc.y };
+                            if (colorspace.filter((csc, idx) => { return csc.x == nc.x && csc.y == nc.y; }, this).length == 0)
+                                stack.push(nc);
+                        }
 
-                        this.points += colorspace.length;
+                        // check east
+                        if ((ccc.x < this.TILE_COLUMNS - 1) && (this.field[ccc.x + 1][ccc.y] && this.field[ccc.x + 1][ccc.y].color == fval)) {
+                            var nc: FieldCoord = { x: ccc.x + 1, y: ccc.y };
+                            if (colorspace.filter((csc, idx) => { return csc.x == nc.x && csc.y == nc.y; }, this).length == 0)
+                                stack.push(nc);
+                        }
                     }
-                }
 
-                if (bFieldChecked) {
-                    console.log(this.field);
-                    break;
-                }
-                else {
-                    // this.ShiftFieldBlocks();
-                    this.field.forEach((line, fldidx) => {
-                        line = line.filter((val) => { return val.color >= 0; });
-                        line.forEach((val, idx) => {
-                            var spr = this.fieldSprites.filter((s) => { return s.key == val.key; }, this)[0].sprt;
-                            spr.position.y = this.SCREEN_HEIGHT - this.TILE_SPACE - (idx * (this.TILE_SPACE + this.TILE_SIZE));
-                        }, this);
-                        this.field[fldidx] = line;
+                    if (colorspace.length < 3)
+                        continue;
+
+                    colorspace.forEach((ccc) => {
+                        let fval = this.field[ccc.x][ccc.y];
+                        fval.color = -1;
+                        let spr = this.GetSprite(fval.key);
+                        this.TweenSpriteAlpha(spr, null, this.RemoveEmptySpaces);
+                        spr.destroy();
                     }, this);
+
+                    localPoints += colorspace.length;
                 }
             }
 
-            // update maxRow
-            for (var c = 0; c < this.TILE_COLUMNS; c++) {
-                this.maxRow = Math.max(this.field[c].filter((num) => { return num.color >= 0; }).length, this.maxRow);
+            if (localPoints == 0) { // field not contain something to remove
+                this.FinishUpdate();
+            } else {
+                this.points += localPoints;
+                this.textValue.text = this.points.toString();
             }
+        }
 
-            // spawn new row
+        TweenSpritePosition(spr: Phaser.Sprite, newX: number, newY: number, onStartCB?: Function, onCompleteCB?: Function) {
+            let emptyAnimation = spr.position.x == newX && spr.position.y == newY;
+            let tw = this.game.add.tween(spr).to(
+                { x: newX, y: newY },
+                100,
+                Phaser.Easing.Linear.None);
+
+            this.TweenSprite(tw, onStartCB, onCompleteCB);
+        }
+
+        TweenSpriteAlpha(spr: Phaser.Sprite, onStartCB?: Function, onCompleteCB?: Function) {
+            console.log("TweenAlpha");
+            let tw = this.game.add.tween(spr).to(
+                { scale: .01 },
+                100, Phaser.Easing.Linear.None);
+
+            this.TweenSprite(tw, onStartCB, onCompleteCB);
+        }
+
+        TweenSprite(tw: Phaser.Tween, onStartCB?: Function, onCompleteCB?: Function) {
+            if (onStartCB === null || typeof onStartCB != 'function')
+                onStartCB = () => { this.lockInput = true; };
+            tw.onStart.addOnce(() => { this.aw.addRef(onStartCB, this); }, this);
+
+            if (onCompleteCB === null || typeof onCompleteCB != 'function')
+                onCompleteCB = () => { this.lockInput = false; };
+            tw.onComplete.addOnce(() => { this.aw.releaseRef(onCompleteCB, this); }, this);
+
+            tw.start();
+        }
+
+        GetSprite(key: number, splice = false): Phaser.Sprite {
+            let idx = -1;
+            let spr = this.fieldSprites.filter((val, i) => {
+                if (key == val.key) {
+                    idx = i;
+                    return true;
+                }
+                return false;
+            }, this)[0].sprt;
+            if (splice)
+                this.fieldSprites.splice(idx, 1);
+            return spr;
+        }
+
+        FinishUpdate() {
+            // update max height
+            for (let c = 0; c < this.TILE_COLUMNS; c++) {
+                this.maxRow = Math.max(this.field[c].length, this.maxRow);
+            }
+            this.lockInput = false;
             this.spawned = false;
-
-            // update score
-            this.textValue.text = this.points.toString();
         }
 
         preload() {
@@ -308,7 +382,7 @@
             this.fieldSprites = [];
 
             this.cursors = this.game.input.keyboard.createCursorKeys();
-            this.cursors.down.onDown.add(SimpleGame.prototype.DropRowDown, this);
+            this.cursors.down.onDown.add(SimpleGame.prototype.DropBlocks, this);
             this.cursors.left.onDown.add(SimpleGame.prototype.ShiftRowLeft, this);
             this.cursors.right.onDown.add(SimpleGame.prototype.ShiftRowRight, this);
 
@@ -320,13 +394,14 @@
 
             this.maxRow = 0;
 
-            var style = { font: "bold 65px Courier", fill: "#ff0000", align: "right" };
+            let style = { font: "bold 65px Courier", fill: "#ff0000", align: "right" };
             this.textValue = this.game.add.text(0, 0, "0", style);
         }
 
         update() {
             if (this.maxRow >= this.TILE_ROWS) {
                 // game over
+                // play animation (remove all blocks in some way)
                 this.game.state.start("EndGameState", true, false, this.points);
                 return;
             }
@@ -334,21 +409,17 @@
             // spawn
             if (!this.spawned) {
                 this.row = [];
-                for (var s = 0; s < this.TILE_COLUMNS; s++) {
-                    var spr = this.game.add.sprite(
+                for (let s = 0; s < this.TILE_COLUMNS; s++) {
+                    let spr = this.game.add.sprite(
                         this.OFFSET_FIELD + this.TILE_SPACE + s * (this.TILE_SPACE + this.TILE_SIZE),
-                        this.TILE_SPACE,
+                        -(this.TILE_SPACE + this.TILE_SIZE),
                         "b" + this.game.rnd.between(0, 5));
 
                     this.row.push(spr);
-                    //                this.game.add.tween(spr.position.x).to(1, 2000, Phaser.Easing.Bounce.In, true);
+                    this.TweenSpritePosition(spr, spr.position.x, this.TILE_SPACE);
                 }
 
                 this.spawned = true;
-            }
-
-            if (this.game.input.activePointer.isDown) {
-                this.game.input.mousePointer.x, this.game.input.mousePointer.y;
             }
 
             this.game.input.reset();
