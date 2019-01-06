024b1be1c3aa52e6d8a7b1ad10d75b86a02a3056
A <fregate@ngs.ru>
2019-01-06 20:56:59 +0700

    moving forest
    remove submodule

--- 2019/w/index.js
+++ 2019/w/index.js
@@ -10,6 +10,33 @@ Math.degrees = function (radians) {
     return radians * 180 / Math.PI;
 };
 
+var sledgeMesh;
+var treeMesh;
+var lastTreeMeshZPosition = 0;
+var treeInstances = 0;
+
+var speedIncrement = 0.01; // speed = currentSpeed + speedIncrement * Math.cos(sledge.rotation.y);
+
+function createTreeRow(scene, offsetZ) {
+    var mr = scene.getMeshByName("tree0" + Math.ceil(Math.random() + .5));
+    var mir = mr.createInstance("itr" + treeInstances);
+    mir.parent = treeMesh;
+    mir.position = new BABYLON.Vector3(-5, 10, offsetZ);
+    mir.rotation = new BABYLON.Vector3(0, Math.PI / 3, 0);
+    mir.scaling.scaleInPlace(.8);
+
+    treeInstances++;
+
+    var ml = scene.getMeshByName("tree0" + Math.ceil(Math.random() + .5));
+    var mil = ml.createInstance("itr" + treeInstances);
+    mil.parent = treeMesh;
+    mil.position = new BABYLON.Vector3(5, 10, offsetZ);
+    mil.rotation = new BABYLON.Vector3(0, Math.PI / 3, 0);
+    mil.scaling.scaleInPlace(.8);
+
+    treeInstances++;
+}
+
 function init() {
     //Init the engine
     var engine = initEngine();
@@ -31,14 +58,27 @@ function init() {
     }));
 
     scene.registerAfterRender(function () {
-        var mainMesh = scene.getMeshByName("sledge");
+        if (sledgeMesh == null)
+            return;
+
         if ((map["q"] || map["Q"])) {
-            mainMesh.rotation.y = clamp(mainMesh.rotation.y + Math.PI / 36, -Math.PI / 2 + Math.PI / 8, Math.PI / 2 - Math.PI / 8);
+            sledgeMesh.rotation.y = clamp(sledgeMesh.rotation.y + Math.PI / 36, -Math.PI / 2 + Math.PI / 8, Math.PI / 2 - Math.PI / 8);
 //            mainMesh.material.diffuseColor.copyFromFloats(Math.random(), Math.random(), Math.random());
         } else if ((map["E"] || map["e"])) {
-            mainMesh.rotation.y = clamp(mainMesh.rotation.y - Math.PI / 36, -Math.PI / 2 + Math.PI / 8, Math.PI / 2 - Math.PI / 8);
+            sledgeMesh.rotation.y = clamp(sledgeMesh.rotation.y - Math.PI / 36, -Math.PI / 2 + Math.PI / 8, Math.PI / 2 - Math.PI / 8);
 //            mainMesh.material.diffuseColor.copyFromFloats(Math.random(), Math.random(), Math.random());
         };
+    });
+
+    scene.registerBeforeRender(function () {
+        treeMesh.position.z -= speedIncrement;
+
+        if (lastTreeMeshZPosition - treeMesh.position.z < 2)
+            return;
+
+        lastTreeMeshZPosition = treeMesh.position.z;
+
+        createTreeRow(scene, 32 - treeMesh.position.z);
     });
 
 /*
@@ -99,6 +139,9 @@ function createScene(engine) {
     var scene = new BABYLON.Scene(engine);
     var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(-1, 1, 1), scene);
     light.groundColor = new BABYLON.Color3(1, 1, 1);
+
+//    var shadowGenerator = new BABYLON.ShadowGenerator(1024, light);
+
     engine.enableOfflineSupport = false;
 
     var camera = createCamera(scene);
@@ -116,7 +159,8 @@ function createScene(engine) {
     var sledgeTask = assetsManager.addMeshTask("sledge mesh", "", "./assets/", "sledge.babylon");
     // You can handle success and error on a per-task basis (onSuccess, onError)
     sledgeTask.onSuccess = function (task) {
-        task.loadedMeshes[0].position = new BABYLON.Vector3(0, 0, -5);
+        sledgeMesh = task.loadedMeshes[0];
+        sledgeMesh.position = new BABYLON.Vector3(0, 0, -5);
     }
 
     var t = assetsManager.addMeshTask("trees", "", "./assets/", "trees.babylon");
@@ -132,12 +176,9 @@ function createScene(engine) {
     }
 
     assetsManager.onFinish = function (tasks) {
-        console.log("loading finished");
-
-        for (var i = 0; i < 40; i++) {
-            var m = scene.getMeshByName("tree0" + Math.ceil(Math.random() + .5));
-            var mi = m.createInstance("itr" + i);
-            mi.position = new BABYLON.Vector3((i % 2) == 0 ? -5 : 5, 0, -40 + Math.ceil(i / 2) * 4);
+        // create start scene
+        for (var i = -8; i < 34; i += 2) {
+            createTreeRow(scene, treeMesh.position.z + i);
         }
 
         engine.runRenderLoop(function () {
@@ -156,7 +197,16 @@ function createScene(engine) {
     snowMaterial.diffuseColor = new BABYLON.Color3(1, 1, 1);
     floor.material = snowMaterial;
     floor.receiveShadows = true;
+    //floor.visibility = 0;
 
+    treeMesh = BABYLON.MeshBuilder.CreateBox("dummy", {}, scene);
+    treeMesh.position.y = -10;
+    var dm = new BABYLON.StandardMaterial("dm_M", scene);
+    dm.diffuseColor = new BABYLON.Color3(1, 0, 1);
+    treeMesh.material = dm;
+    lastTreeMeshZPosition = treeMesh.position.z;
+
+    scene.debugLayer.show();
     return scene;
 }
 
