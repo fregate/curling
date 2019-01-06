7a1125d8ad7497e410e7040e2fbfc180356bb3d5
A <fregate@ngs.ru>
2019-01-06 16:22:01 +0700

    Initial commit for 2019
    Few objects, babylon

---
+++ 2019/w/index.js
@@ -0,0 +1,170 @@
+﻿///<reference path="scripts/babylon.max.js" />
+
+// Converts from degrees to radians.
+Math.radians = function (degrees) {
+    return degrees * Math.PI / 180;
+};
+
+// Converts from radians to degrees.
+Math.degrees = function (radians) {
+    return radians * 180 / Math.PI;
+};
+
+function init() {
+    //Init the engine
+    var engine = initEngine();
+    //Create a new scene
+    var scene = createScene(engine);
+
+    /****************************Key Control Multiple Keys************************************************/
+
+    var map = {}; //object for multiple key presses
+    scene.actionManager = new BABYLON.ActionManager(scene);
+
+    scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyDownTrigger, function (evt) {
+        map[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
+
+    }));
+
+    scene.actionManager.registerAction(new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnKeyUpTrigger, function (evt) {
+        map[evt.sourceEvent.key] = evt.sourceEvent.type == "keydown";
+    }));
+
+    scene.registerAfterRender(function () {
+        var mainMesh = scene.getMeshByName("sledge");
+        if ((map["q"] || map["Q"])) {
+            mainMesh.rotation.y = clamp(mainMesh.rotation.y + Math.PI / 36, -Math.PI / 2 + Math.PI / 8, Math.PI / 2 - Math.PI / 8);
+//            mainMesh.material.diffuseColor.copyFromFloats(Math.random(), Math.random(), Math.random());
+        } else if ((map["E"] || map["e"])) {
+            mainMesh.rotation.y = clamp(mainMesh.rotation.y - Math.PI / 36, -Math.PI / 2 + Math.PI / 8, Math.PI / 2 - Math.PI / 8);
+//            mainMesh.material.diffuseColor.copyFromFloats(Math.random(), Math.random(), Math.random());
+        };
+    });
+
+/*
+    var showAxis = function (size) {
+        var makeTextPlane = function (text, color, size) {
+            var dynamicTexture = new BABYLON.DynamicTexture("DynamicTexture", 50, scene, true);
+            dynamicTexture.hasAlpha = true;
+            dynamicTexture.drawText(text, 5, 40, "bold 36px Arial", color, "transparent", true);
+            var plane = new BABYLON.Mesh.CreatePlane("TextPlane", size, scene, true);
+            plane.material = new BABYLON.StandardMaterial("TextPlaneMaterial", scene);
+            plane.material.backFaceCulling = false;
+            plane.material.specularColor = new BABYLON.Color3(0, 0, 0);
+            plane.material.diffuseTexture = dynamicTexture;
+            return plane;
+        };
+
+        var axisX = BABYLON.Mesh.CreateLines("axisX", [
+            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, 0.05 * size, 0),
+            new BABYLON.Vector3(size, 0, 0), new BABYLON.Vector3(size * 0.95, -0.05 * size, 0)
+        ], scene);
+        axisX.color = new BABYLON.Color3(1, 0, 0);
+        var xChar = makeTextPlane("X", "red", size / 10);
+        xChar.position = new BABYLON.Vector3(0.9 * size, -0.05 * size, 0);
+        var axisY = BABYLON.Mesh.CreateLines("axisY", [
+            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(-0.05 * size, size * 0.95, 0),
+            new BABYLON.Vector3(0, size, 0), new BABYLON.Vector3(0.05 * size, size * 0.95, 0)
+        ], scene);
+        axisY.color = new BABYLON.Color3(0, 1, 0);
+        var yChar = makeTextPlane("Y", "green", size / 10);
+        yChar.position = new BABYLON.Vector3(0, 0.9 * size, -0.05 * size);
+        var axisZ = BABYLON.Mesh.CreateLines("axisZ", [
+            new BABYLON.Vector3.Zero(), new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, -0.05 * size, size * 0.95),
+            new BABYLON.Vector3(0, 0, size), new BABYLON.Vector3(0, 0.05 * size, size * 0.95)
+        ], scene);
+        axisZ.color = new BABYLON.Color3(0, 0, 1);
+        var zChar = makeTextPlane("Z", "blue", size / 10);
+        zChar.position = new BABYLON.Vector3(0, 0.05 * size, 0.9 * size);
+    };
+    showAxis(7);
+    */
+}
+
+function initEngine() {
+    // Get the canvas element from index.html
+    var canvas = document.getElementById("renderCanvas");
+    // Initialize the BABYLON 3D engine
+    var engine = new BABYLON.Engine(canvas, true);
+
+    // Watch for browser/canvas resize events
+    window.addEventListener("resize", function () {
+        engine.resize();
+    });
+
+    return engine;
+}
+
+function createScene(engine) {
+    var scene = new BABYLON.Scene(engine);
+    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(-1, 1, 1), scene);
+    light.groundColor = new BABYLON.Color3(1, 1, 1);
+    engine.enableOfflineSupport = false;
+
+    var camera = createCamera(scene);
+    camera.mode = BABYLON.Camera.ORTHOGRAPHIC_CAMERA;
+    camera.orthoTop = 20;
+    camera.orthoBottom = -25;
+    camera.orthoLeft = -20;
+    camera.orthoRight = 20;
+
+//  camera.attachControl(document.getElementById("renderCanvas"), false);
+
+    // Assets manager
+    var assetsManager = new BABYLON.AssetsManager(scene);
+
+    var sledgeTask = assetsManager.addMeshTask("sledge mesh", "", "./assets/", "sledge.babylon");
+    // You can handle success and error on a per-task basis (onSuccess, onError)
+    sledgeTask.onSuccess = function (task) {
+        task.loadedMeshes[0].position = new BABYLON.Vector3(0, 0, -5);
+    }
+
+    var t = assetsManager.addMeshTask("trees", "", "./assets/", "trees.babylon");
+    t.onSuccess = function (task) {
+        task.loadedMeshes.forEach(function (mesh, idx, arr) {
+            mesh.position = new BABYLON.Vector3(0, -10, 0);
+        });
+    }
+
+    // But you can also do it on the assets manager itself (onTaskSuccess, onTaskError)
+    assetsManager.onTaskError = function (task) {
+        console.log("error while loading " + task.name);
+    }
+
+    assetsManager.onFinish = function (tasks) {
+        console.log("loading finished");
+
+        for (var i = 0; i < 40; i++) {
+            var m = scene.getMeshByName("tree0" + Math.ceil(Math.random() + .5));
+            var mi = m.createInstance("itr" + i);
+            mi.position = new BABYLON.Vector3((i % 2) == 0 ? -5 : 5, 0, -40 + Math.ceil(i / 2) * 4);
+        }
+
+        engine.runRenderLoop(function () {
+            scene.render();
+        });
+    };
+
+    // Can now change loading background color:
+    engine.loadingUIBackgroundColor = "Purple";
+
+    // Just call load to initiate the loading sequence
+    assetsManager.load();
+
+    var floor = BABYLON.MeshBuilder.CreateGround("floor", { width: 200, height: 400, subdivisions: 1, updatable: false }, scene);
+    var snowMaterial = new BABYLON.StandardMaterial(name, scene);
+    snowMaterial.diffuseColor = new BABYLON.Color3(1, 1, 1);
+    floor.material = snowMaterial;
+    floor.receiveShadows = true;
+
+    return scene;
+}
+
+function createCamera(scene) {
+    var camera = new BABYLON.ArcRotateCamera("cam", Math.PI / 2, Math.radians(30), 70, new BABYLON.Vector3(0, 0, 0), scene);
+    return camera;
+}
+
+function clamp(value, vmin, vmax) {
+    return Math.min(vmax, Math.max(vmin, value));
+}
