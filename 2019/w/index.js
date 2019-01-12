89c91e2d0e2e6247864ed4092ea7217080580842
A <fregate@ngs.ru>
2019-01-12 18:49:10 +0700

    Correct shadows and remove invisible meshes
    https://forum.babylonjs.com/t/odd-shadowmap-rendering/706

--- 2019/w/index.js
+++ 2019/w/index.js
@@ -1,4 +1,4 @@
-﻿///<reference path="scripts/babylon.max.js" />
+///<reference path="scripts/babylon.max.js" />
 
 // Converts from degrees to radians.
 Math.radians = function (degrees) {
@@ -10,14 +10,16 @@ Math.degrees = function (radians) {
     return radians * 180 / Math.PI;
 };
 
+var theWall;
 var sledgeMesh;
 var treeMesh;
 var lastTreeMeshZPosition = 0;
-var meshInstanses = 0;
 var treeOffset = 7;
 
+var meshInstanses = 0;
+
 var currentSpeed = 0.01;
-var speedIncrement = 0.002; // forest speed = currentSpeed + speedIncrement * Math.cos(sledge.rotation.y);
+var speedIncrement = 0.002;
 var currentSledgeSpeed = 0;
 var speedSledgeIncrement = 0.1;
 
@@ -34,15 +36,28 @@ function meshInstantinate(mesh, name, pos, rot, s) {
     m.rotation = rot;
     m.scaling.scaleInPlace(s);
     m.checkCollisions = true;
+    theWall.actionManager.registerAction(
+        new BABYLON.ExecuteCodeAction(
+            {
+                trigger: BABYLON.ActionManager.OnIntersectionExitTrigger,
+                parameter: m
+            },
+            function (evt) {
+                evt.additionalData.dispose();
+            }
+        )
+    );
     return m;
 }
 
 function createTreeRow(scene, offsetZ) {
     var mr = scene.getMeshByName("tree0" + Math.ceil(Math.random() + .5));
-    meshInstantinate(mr, "tr", new BABYLON.Vector3(-treeOffset, 10, offsetZ), new BABYLON.Vector3(0, Math.PI / 3, 0), .8);
+    var mi = meshInstantinate(mr, "tr", new BABYLON.Vector3(-treeOffset, 10, offsetZ), new BABYLON.Vector3(0, Math.PI / 3, 0), .8);
+    shadowGenerator.addShadowCaster(mi);
 
     var ml = scene.getMeshByName("tree0" + Math.ceil(Math.random() + .5));
-    meshInstantinate(ml, "tr", new BABYLON.Vector3(treeOffset, 10, offsetZ), new BABYLON.Vector3(0, Math.PI / 3, 0), .8);
+    mi = meshInstantinate(ml, "tr", new BABYLON.Vector3(treeOffset, 10, offsetZ), new BABYLON.Vector3(0, Math.PI / 3, 0), .8);
+    shadowGenerator.addShadowCaster(mi);
 }
 
 function init() {
@@ -71,28 +86,23 @@ function init() {
 
         if ((map["q"] || map["Q"])) {
             sledgeMesh.rotation.y = clamp(sledgeMesh.rotation.y + Math.PI / 36, -Math.PI / 2 + Math.PI / 8, Math.PI / 2 - Math.PI / 8);
-//            mainMesh.material.diffuseColor.copyFromFloats(Math.random(), Math.random(), Math.random());
             currentSledgeSpeed = speedSledgeIncrement * Math.sin(sledgeMesh.rotation.y);
         } else if ((map["E"] || map["e"])) {
             sledgeMesh.rotation.y = clamp(sledgeMesh.rotation.y - Math.PI / 36, -Math.PI / 2 + Math.PI / 8, Math.PI / 2 - Math.PI / 8);
-//            mainMesh.material.diffuseColor.copyFromFloats(Math.random(), Math.random(), Math.random());
             currentSledgeSpeed = speedSledgeIncrement * Math.sin(sledgeMesh.rotation.y);
         };
     });
 
     scene.registerBeforeRender(function () {
-        treeMesh.position.z -= currentSpeed;
-
-//        sledgeMesh.position.x += currentSledgeSpeed;
-        var forwards = new BABYLON.Vector3(currentSledgeSpeed, 0, 0);
-        sledgeMesh.moveWithCollisions(forwards);
+        treeMesh.moveWithCollisions(new BABYLON.Vector3(0, 0, -currentSpeed));
+        sledgeMesh.moveWithCollisions(new BABYLON.Vector3(currentSledgeSpeed, 0, 0));
 
         if (lastTreeMeshZPosition - treeMesh.position.z < 2)
             return;
 
         lastTreeMeshZPosition = treeMesh.position.z;
 
-        createTreeRow(scene, 32 - treeMesh.position.z);
+        createTreeRow(scene, 36 - treeMesh.position.z);
 
         // increase speed
         currentSpeed = clamp(currentSpeed + speedIncrement * Math.cos(sledgeMesh.rotation.y), 0, 1);
@@ -116,22 +126,21 @@ function initEngine() {
 function createScene(engine) {
     var scene = new BABYLON.Scene(engine);
     scene.shadowsEnabled = true;
-//    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 1), scene);
-//    light.groundColor = new BABYLON.Color3(1, 1, 1);
     var light = new BABYLON.DirectionalLight("light", new BABYLON.Vector3(0, -1, -1), scene);
+    light.position = new BABYLON.Vector3(10, 10, 30);
+    light.setDirectionToTarget(new BABYLON.Vector3(0, 0, 0));
     light.specular = new BABYLON.Color3(0, 0, 0);
-    light.intensity = 1.2;
+    light.intensity = 1.7;
 
     var light2 = new BABYLON.DirectionalLight("light2", new BABYLON.Vector3(0, -1, -1), scene);
+    light2.position = new BABYLON.Vector3(-10, 70, -30);
+    light2.setDirectionToTarget(new BABYLON.Vector3(0, 0, 0));
     light2.specular = new BABYLON.Color3(0, 0, 0);
-    light2.intensity = 1.7;
+    light2.intensity = 1.5;
 
-    shadowGenerator = new BABYLON.ShadowGenerator(1024, light2);
-    shadowGenerator.useBlurExponentialShadowMap = true;
-    //shadowGenerator.usePoissonSampling = true;
+    shadowGenerator = new BABYLON.ShadowGenerator(2048, light2);
+    shadowGenerator.useVarianceShadowMap = true;
     shadowGenerator.blurBoxOffset = 2.0;
-
-//    engine.enableOfflineSupport = false;
 
     var camera = createCamera(scene);
     camera.mode = BABYLON.Camera.ORTHOGRAPHIC_CAMERA;
@@ -146,14 +155,12 @@ function createScene(engine) {
     var assetsManager = new BABYLON.AssetsManager(scene);
 
     var sledgeTask = assetsManager.addMeshTask("sledge mesh", "", "./assets/", "sledge.babylon");
-    // You can handle success and error on a per-task basis (onSuccess, onError)
     sledgeTask.onSuccess = function (task) {
         sledgeMesh = task.loadedMeshes[0];
         sledgeMesh.position = new BABYLON.Vector3(0, 0, -5);
         sledgeMesh.checkCollisions = true;
 
         sledgeMesh.onCollide = function (cm) {
-            console.log(cm.name);
             if (cm.name.startsWith(starName)) {
                 // pick star (reduce speed by half)
             } else if (cm.name.startsWith(giftName)) {
@@ -178,6 +185,7 @@ function createScene(engine) {
     g.onSuccess = function (task) {
         task.loadedMeshes.forEach(function (mesh, idx, arr) {
             mesh.convertToFlatShadedMesh();
+            mesh.position = new BABYLON.Vector3(0, -10, 0);
         });
     }
 
@@ -188,7 +196,7 @@ function createScene(engine) {
 
     assetsManager.onFinish = function (tasks) {
         // create start scene
-        for (var i = -8; i <= 32; i += 2) {
+        for (var i = -8; i <= 36; i += 2) {
             createTreeRow(scene, treeMesh.position.z + i);
         }
 
@@ -218,7 +226,12 @@ function createScene(engine) {
     treeMesh.position.y = -10;
     lastTreeMeshZPosition = treeMesh.position.z;
 
-    scene.debugLayer.show();
+    theWall = BABYLON.MeshBuilder.CreatePlane("theWall", { sideOrientation: BABYLON.Mesh.DOUBLESIDE, updatable: true, width: treeOffset * 4, height: 1 }, scene);
+    theWall.position = new BABYLON.Vector3(0, 0, -30);
+    theWall.material = new BABYLON.StandardMaterial("debug_M", scene);
+    theWall.material.diffuseColor = new BABYLON.Color3.Red();
+    theWall.checkCollisions = true;
+    theWall.actionManager = new BABYLON.ActionManager(scene);
     return scene;
 }
 
